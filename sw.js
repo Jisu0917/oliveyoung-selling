59730fbcd779386a4cab6168cf72953240e74e01
Park Jisu <57223927+Jisu0917@users.noreply.github.com>
2026-09-04 13:46:27 +0900

    버그 재수정
    바코드 인식 버그 재수정

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='oy-da22-v4-fix1';
+const CACHE='oy-da22-v4-fix2';
 const ASSETS=['./','./index.html','./oliveyoung_DA22_runtime_db_v4.json','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
